2864ac478927cd5e0babc208805955f90591803c
donteghen <donaldteghen@gmail.com>
2024-09-01 08:25:03 +0100

    completed the spaceship game

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
-const {range, map, toArray, take, mergeMap, interval, tap, fromEvent, startWith, combineLatest, scan, sample, sampleTime} = rxjs;
-//const {range, map, toArray, take, mergeMap, interval, tap, fromEvent, startWith, combineLatest, scan, sample, sampleTime} = require('rxjs');
+const {Subject, concat, range, map, toArray, take, mergeMap, interval, tap, fromEvent, startWith, combineLatest, scan, sample,
+     sampleTime, filter, timestamp, merge, distinctUntilChanged, debounceTime, takeWhile, of} = rxjs;
+//const {range, map, toArray, take, mergeMap, interval, tap, fromEvent, startWith, combineLatest, scan, sample, sampleTime, filter, timestamp, merge, distinctUntilChanged} = require('rxjs');
 //require('dotenv').config();
-
+//const R = require('ramda');
 //  import other local modules
 //const {generateStarBg} = require('./src/util/star');
 
+const startBtn = document.querySelector('#gamer-button');
 const canva = document?.createElement('canvas');
+canva.tabIndex = 1; // work-around to make the canva element focusable
 const context = canva.getContext('2d');
 
 document?.body?.appendChild(canva);
 canva.width = window?.innerWidth;
 canva.height = window?.innerHeight;
 
+// Simulate canvas click when button is clicked
+startBtn.addEventListener('click', () => {
+    // remove button from view
+    document.body.removeChild(startBtn);
+    // Create a new MouseEvent for the canvas click
+    const canvasClickEvent = new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true        
+    });
+
+    // Dispatch the event on the canvas
+    canva.dispatchEvent(canvasClickEvent);
+});
+
 // to be refactored 
 const SPEED = 50;
 const STAR_NUMBER = 250;
-const ENEMY_FREQ = 1500;
+const ENEMY_FREQ = 3000 //1500;
 const HERO_Y = canva.height - 30;
 const HERO_COLOR = '#ff0000';
 const ENEMY_Y = -30;
@@ -30,6 +47,11 @@ const ENEMY_SHIP_FLIGHT_MODE = {
     NORMAL_MODE : 'normal',
     GHOST_MODE: 'ghost'
 }
+const BULLET_SIZE = 5;
+const SHOOOTING_SPEED = 15;
+const ENEMY_SHOOT_FREQ = 750;
+const SCORE_INCREASE = 10;
+const COLLISION_PROXIMITY = 5;
 
 // TO DO: Move to helper file.*****************************************
 const mouseMoveEventToNewPos = e => ({x: e.clientX, y: HERO_Y});
@@ -48,13 +70,18 @@ const enemyAccumilator = (enemies) => {
     //console.log('enemies', enemies)
     const newEnemy = {
         x: parseInt(Math.random() * canva.width),
-        y: ENEMY_Y
-    } 
+        y: ENEMY_Y,
+        shots: []
+    } ;
+    interval(ENEMY_SHOOT_FREQ).subscribe(v => {
+        newEnemy.shots.push({x:newEnemy.x, y: newEnemy.y})
+    });
+    const aliveOrHasShots = (enemy) => !(enemy.isDead && enemy.shots.length === 0);
     enemies.push(newEnemy);
-    return enemies;
+    return enemies.filter(aliveOrHasShots);
 }
 
-const enemy$ = interval(ENEMY_FREQ).pipe(
+const enemies$ = interval(ENEMY_FREQ).pipe(
     scan(enemyAccumilator, [])
 )
 /**
@@ -68,6 +95,18 @@ const paintStars = (stars) => {
     stars.forEach(star => {
         context.fillRect(star.posX, star.posY, star.size, star.size)
     });
+}
+
+const subject = new Subject()
+const scoreSubject$ = subject.pipe(
+    scan((acc, prev) => acc + prev, 0),
+    startWith(0)
+)
+
+const paintScore = (score) => {
+    context.fillStyle = '#fffff';
+    context.font = 'bold 26px sans-serif';
+    context.fillText(`Score : ${score}`, 40, 43);
 }
 
 
@@ -94,18 +133,14 @@ function generateStarBg$ (canva, starCount) {
                 size : Number.parseInt(1 + Math.random() * 3)
             } 
         }),
-        //take(4),
-        toArray(),
-        //tap(v => {console.log(v)})
+        toArray()
     )
     .pipe(
         mergeMap(stars => {
-            //console.log(stars)
             return interval(SPEED)
             .pipe(
                 map(() => {
                     stars.forEach(star => {
-                        //console.log('star.posY >= canva.height', star.posY, canva.height, star.posY >= canva.height)
                         if (star.posY >= canva.height) {
                             star.posY = 0;
                         }
@@ -120,17 +155,17 @@ function generateStarBg$ (canva, starCount) {
     )
 }
 
-
 const drawTriangle = (x, y, width, color, direction) => {
     context.fillStyle = color;
     context.beginPath();
     context.moveTo(x - width, y);
-    context.lineTo(x, direction === 'up' ? y - width : y + width); context.lineTo(x + width, y);
+    context.lineTo(x, direction === 'up' ? y - width : y + width); 
+    context.lineTo(x + width, y);
     context.lineTo(x - width,y);
     context.fill();
 }
 
-
+// ships settup and control
 function paintSpaceShip({x, y}) { 
     drawTriangle(x, y, SHIP_SIZE, HERO_COLOR, SHIP_POINT_DIRECTION_UP);
 }
@@ -141,35 +176,99 @@ function paintEnemyShips(enemies, mode) {
     enemies.forEach(enemy => {
         enemy.y += ENEMY_ACCELERATION;
         enemy.x = enemyShipControl(enemy.x, mode)
-        drawTriangle(enemy.x, enemy.y, 10, ENEMY_COLOR, SHIP_POINT_DIRECTION_DOWN);
+        if (!enemy.isDead) {
+            drawTriangle(enemy.x, enemy.y, SHIP_SIZE, ENEMY_COLOR, SHIP_POINT_DIRECTION_DOWN);
+        }
+        enemy.shots.forEach(shot => {
+            shot.y += SHOOOTING_SPEED;
+            drawTriangle(shot.x, shot.y, BULLET_SIZE, ENEMY_COLOR, SHIP_POINT_DIRECTION_DOWN);
+        })
     });    
 }
 
-function enemyShipControl (oldPositionX, mode) {
-    const isGhostMode = mode === ENEMY_SHIP_FLIGHT_MODE.NORMAL_MODE ;
+function enemyShipControl (oldPositionX, mode) {    
+    const isGhostMode = mode === ENEMY_SHIP_FLIGHT_MODE.GHOST_MODE ;
     const newPositionX = oldPositionX + getRandomGlide(ENEMY_LEFT_GLIDE, ENEMY_RIGHT_GLIDE);
     if (!isGhostMode) {        
         return newPositionX < 0 ? 4 : newPositionX > canva.width ? canva.width - 4 : newPositionX;
     }
     return newPositionX < 0 ? (newPositionX + canva.width) : newPositionX > canva.width ? (newPositionX - canva.width) : newPositionX;
 }
-const renderScene = (mode) => (actors) => {
-    //console.log('actors', actors.stars)
-    paintStars(actors.stars);
-    paintSpaceShip(actors.spaceShip);    
-    paintEnemyShips(actors.enemies, mode);    
+
+// shooting and actions
+const click$ = fromEvent(canva, 'click');
+const spacePress$ = fromEvent(canva, 'keydown').pipe(
+    //tap(console.log),
+    filter(R.compose(R.equals(32), R.prop('keyCode')))
+); 
+const heroFiring$ = merge(click$, spacePress$).pipe(    
+    timestamp(),
+    debounceTime(10) 
+);
+
+const combineShotPositionSelector = (heroFiring, spaceShip) => ({x: spaceShip.x, timestamp: heroFiring.timestamp});
+const heroShotAccumilator = (shootArray, shot) => {
+    shootArray.push({x: shot.x, y: HERO_Y});
+    return shootArray;
+}
+const heroShot$ = combineLatest([heroFiring$, spaceShip$], combineShotPositionSelector).pipe(
+    distinctUntilChanged((prev, curr) => prev.timestamp === curr.timestamp),
+    scan(heroShotAccumilator, []),    
+);
+
+
+const paintHeroShoot = (shots, enemies) => {
+    shots.forEach(shot => {
+        enemies.forEach(enemy => {
+            if (!enemy.isDead && collision(shot, enemy)) {
+                scoreSubject$.next(SCORE_INCREASE);
+                enemy.isDead = true;
+                shot.x = shot.y = -100;
+                return
+            }
+        })
+        shot.y -= SHOOOTING_SPEED; 
+        drawTriangle(shot.x, shot.y, BULLET_SIZE, HERO_COLOR, SHIP_POINT_DIRECTION_UP)       
+    })
 }
 
 // start sequence
-const game = combineLatest([generateStarBg$(canva, STAR_NUMBER), spaceShip$, enemy$], function (stars, spaceShip, enemies)  {    
-    //console.log('stars, spaceShip, enemy$', enemies)
-    return {stars, spaceShip, enemies}
-})
+const renderScene = (mode) => (actors) => {    
+    paintStars(actors.stars);
+    paintSpaceShip(actors.spaceShip);    
+    paintEnemyShips(actors.enemies, mode); 
+    paintHeroShoot(actors.shots, actors.enemies);
+    paintScore(actors.score)
+}
 
-function start (mode) {
+const combineSelector = (stars, spaceShip, enemies, shots, score) => ({stars, spaceShip, enemies, shots, score});
+
+const game = combineLatest([generateStarBg$(canva, STAR_NUMBER), spaceShip$, enemies$, heroShot$, scoreSubject$], combineSelector).pipe(
+    takeWhile(actors => !gameOver(actors.spaceShip, actors.enemies))
+)
+
+function start (mode) { 
     game.pipe(
         sampleTime(SPEED)
     ).subscribe(renderScene(mode));
 }
 
 start(ENEMY_SHIP_FLIGHT_MODE.NORMAL_MODE);
+
+
+// Helpers
+function collision(target1, target2) {
+    return (target1.x > target2.x - COLLISION_PROXIMITY && target1.x < target2.x + COLLISION_PROXIMITY) &&
+            (target1.y > target2.y - COLLISION_PROXIMITY && target1.y < target2.y + COLLISION_PROXIMITY);
+}
+
+function gameOver (ship, enemies) {
+    return enemies.some(enemy => {
+        if (collision(ship, enemy)) {
+            return true;
+        }
+        return enemy.shots.some(shot => {            
+            return collision(ship, shot);            
+        })
+    })
+}
